d19c34db0e8150e19275f1c6577671a346c1e53d
Thomas <thomasmcmah21@gmail.com>
2026-01-01 13:51:38 +1000

    Add project files.

---
+++ ImageBrowser/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ImageBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            else
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private Image _originalImage;
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                _originalImage?.Dispose();
+                _originalImage = Image.FromFile(openFileDialog1.FileName);
+
+                pictureBox1.Image = _originalImage;
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+        }
+
+        private void backgroundButton_Click(object sender, EventArgs e)
+        {
+            // Show the color dialog box. If the user clicks OK, change the
+            // PictureBox control's background to the color the user chose.
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
